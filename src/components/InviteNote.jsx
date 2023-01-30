982d4fdf58c555b197c25bc18ebdeec0daea71f7
ziauddinziya07 <ziauddinziya07@gmail.com>
2023-01-30 21:37:10 +0530

    first commit

---
+++ src/components/InviteNote.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/InviteNote.css";
+
+function InviteNote() {
+    return (
+        <section id="invite-note">
+            <div className="from">
+                <h5>An Invitation from:</h5>
+                <h2>Mr. & Mrs. Chanderbande Mohammed Jaffar Quadri</h2>
+
+                <h5 className="address mb-3">
+                    Residence: Silpa Elite Apartments,
+                    <br /> Opposite Chekuri Lodge,
+                    <br /> Adoni, Kurnool Dist.,
+                    <br /> Andhra Pradesh - 518 301.
+                </h5>
+                <p className="address">
+                    Contact: <br />
+                    +91 99492 40408, <br />
+                    +91 91216 13928.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default InviteNote;
